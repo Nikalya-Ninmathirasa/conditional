f38b78c70cae6f3c37160558b8c054d36041f5c5
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-18 12:11:22 +0530

    added more lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conditional';
+
+  isVisible=true;
+  color:string='blue';
+
+  userArray = ['Ram','Rahul','Raj']
+  userList = [
+    {name:'Ram',salary:50000, location:'Jaffna',skillset:['angular','.Net','Java']},
+    {name:'Rahul',salary:60000, location:'PPD',skillset:['Reactjs','Nodejs','Java']},
+    {name:'Raj',salary:70000, location:'Kandy',skillset:['vuejs','springboot','Python']}
+  ]
 }
